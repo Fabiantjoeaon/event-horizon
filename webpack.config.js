0b349bb17bcab54254c3c5105ea962a804990b51
Fabian Tjoe-A-On <fabiantjoeaon@gmail.com>
2017-07-01 21:12:11 +0200

    Basic setup

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+'use strict';
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+const webpack = require('webpack');
+const path = require('path');
+
+const SRC_DIR = path.resolve(__dirname, './src');
+const BUILD_DIR = path.resolve(__dirname, './dist');
+
+const javascript = {
+  test: /\.(js)$/, 
+  use: [{
+    loader: 'babel-loader',
+    options: { presets: ['es2015', 'stage-0'] },
+    query: {compact: false}
+  }],
+};
+
+const postcss = {
+  loader: 'postcss-loader',
+  options: {
+    url: false,
+    plugins() { return [autoprefixer({ browsers: 'last 3 versions' })]; }
+  }
+};
+
+const styles = {
+  test: /\.(scss)$/,
+  use: ExtractTextPlugin.extract(['css-loader?url=false', postcss, 'sass-loader'])
+};
+
+const uglify = new webpack.optimize.UglifyJsPlugin({
+  compress: { warnings: false }
+});
+
+const config = {
+  entry: {
+    App: `${SRC_DIR}/App.js`
+  },
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [javascript, styles]
+  },
+  plugins: [
+    new ExtractTextPlugin('style.css'),
+  ]
+};
+
+process.noDeprecation = true;
+
+module.exports = config;
